a863f108799e942698dfc6054635bf304aece1aa
Itay Maman <imaman@users.noreply.github.com>
2022-04-26 11:44:14 +0300

    _

--- modules/build-raptor-core/src/task-executor.ts
+++ modules/build-raptor-core/src/task-executor.ts
@@ -13,7 +13,6 @@ import { Task } from './task'
 import { TaskStore } from './task-store'
 import { TaskTracker } from './task-tracker'
 
-// TODO(imaman): explain.
 export class TaskExecutor {
   constructor(
     private readonly taskName: TaskName,
