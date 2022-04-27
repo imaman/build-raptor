a8dad87b53518170c42dd6d51a749beb680cf1d8
Itay Maman <imaman@users.noreply.github.com>
2022-04-27 18:35:55 +0300

    _

--- modules/yarn-repo-protocol/src/externals.d.ts
+++ modules/yarn-repo-protocol/src/externals.d.ts
@@ -1,11 +1,4 @@
 declare module 'webpack-shebang-plugin' {
-  // export = Clock;
-
-  // declare class Clock {
-  //   getTime() : number;
-  //   static isClock( value: any ) : boolean;
-  // }
-
   class ShebangPlugin {
     constructor()
   }
