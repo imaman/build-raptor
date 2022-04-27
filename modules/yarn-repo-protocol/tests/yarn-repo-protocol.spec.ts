dbae600bbd1537fae50edc3036512703aaed98c2
Itay Maman <imaman@users.noreply.github.com>
2022-04-27 12:59:23 +0300

    cover

--- modules/yarn-repo-protocol/tests/yarn-repo-protocol.spec.ts
+++ modules/yarn-repo-protocol/tests/yarn-repo-protocol.spec.ts
@@ -7,7 +7,7 @@ import { YarnRepoProtocol } from '../src/yarn-repo-protocol'
 describe('yarn-repo-protocol', () => {
   const logger = createDefaultLogger('/tmp/abc')
   describe('computePackingPackageJson', () => {
-    test('includes all out-of-repo deps of all in-repo deps (sorted)', async () => {
+    test('includes out-of-repo deps of all in-repo deps (sorted)', async () => {
       const d = await folderify({
         'package.json': { workspaces: ['modules/*'], private: true },
         'modules/a/package.json': { name: 'a', version: '1.0.0', dependencies: { b: '1.0.0', foo: '400.1.0' } },
@@ -17,8 +17,7 @@ describe('yarn-repo-protocol', () => {
       const yrp = new YarnRepoProtocol(logger)
       await yrp.initialize(d)
 
-      const p = await yrp.computePackingPackageJson(UnitId('a'))
-      expect(p).toEqual({
+      expect(await yrp.computePackingPackageJson(UnitId('a'))).toMatchObject({
         name: 'a',
         version: '1.0.0',
         dependencies: {
@@ -54,6 +53,40 @@ describe('yarn-repo-protocol', () => {
         version: '1.0.0',
         dependencies: { y: '200.1.0' },
       })
+    })
+  })
+  test('does not include out-of-repo dev-dependencies of an in-repo dep', async () => {
+    const d = await folderify({
+      'package.json': { workspaces: ['modules/*'], private: true },
+      'modules/a/package.json': { name: 'a', version: '1.0.0', dependencies: { b: '1.0.0', c: '1.0.0' } },
+      'modules/b/package.json': { name: 'b', version: '1.0.0', dependencies: { x: '100.1.0' } },
+      'modules/c/package.json': { name: 'c', version: '1.0.0', devDependencies: { y: '200.1.0' } },
+    })
+
+    const yrp = new YarnRepoProtocol(logger)
+    await yrp.initialize(d)
+
+    expect(await yrp.computePackingPackageJson(UnitId('a'))).toEqual({
+      name: 'a',
+      version: '1.0.0',
+      dependencies: { x: '100.1.0' },
+    })
+  })
+  test('does not include dependencies (dev or not) of an in-repo dev-dependency', async () => {
+    const d = await folderify({
+      'package.json': { workspaces: ['modules/*'], private: true },
+      'modules/a/package.json': { name: 'a', version: '1.0.0', devDependencies: { b: '1.0.0' } },
+      'modules/b/package.json': { name: 'b', version: '1.0.0', dependencies: { c: '1.0.0', x: '100.1.0' } },
+      'modules/c/package.json': { name: 'c', version: '1.0.0', devdependencies: { y: '200.1.0' } },
+    })
+
+    const yrp = new YarnRepoProtocol(logger)
+    await yrp.initialize(d)
+
+    expect(await yrp.computePackingPackageJson(UnitId('a'))).toEqual({
+      name: 'a',
+      version: '1.0.0',
+      dependencies: {},
     })
   })
 })
