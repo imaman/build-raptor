b6f4e36609d511e6389d9f6e0d02457ceeece0fd
Itay Maman <imaman@users.noreply.github.com>
2022-04-27 12:22:17 +0300

    test

--- modules/yarn-repo-protocol/tests/yarn-repo-protocol.spec.ts
+++ modules/yarn-repo-protocol/tests/yarn-repo-protocol.spec.ts
@@ -6,47 +6,31 @@ import { YarnRepoProtocol } from '../src/yarn-repo-protocol'
 
 describe('yarn-repo-protocol', () => {
   const logger = createDefaultLogger('/tmp/abc')
-  test('finds all deps', async () => {
-    const d = await folderify({
-      'package.json': {
-        workspaces: ['modules/*'],
-        private: true,
-      },
-      'modules/a/package.json': {
+  describe('computePackingPackageJson', () => {
+    test('includes all out-of-repo deps of all in-repo deps', async () => {
+      const d = await folderify({
+        'package.json': { workspaces: ['modules/*'], private: true },
+        'modules/a/package.json': { name: 'a', version: '1.0.0', dependencies: { b: '1.0.0', foo: '400.1.0' } },
+        'modules/b/package.json': {
+          name: 'b',
+          version: '1.0.0',
+          dependencies: { b: '1.0.0', goo: '100.1.0', boo: '200.1.0' },
+        },
+      })
+
+      const yrp = new YarnRepoProtocol(logger)
+      await yrp.initialize(d)
+
+      const p = await yrp.computePackingPackageJson(UnitId('a'))
+      expect(p).toEqual({
         name: 'a',
         version: '1.0.0',
         dependencies: {
-          b: '1.0.0',
-          foo: '400.1.0',
-        },
-      },
-      'modules/b/package.json': {
-        name: 'b',
-        version: '1.0.0',
-        dependencies: {
-          b: '1.0.0',
-          goo: '100.1.0',
           boo: '200.1.0',
+          foo: '400.1.0',
+          goo: '100.1.0',
         },
-      },
+      })
     })
-
-    const yrp = new YarnRepoProtocol(logger)
-    await yrp.initialize(d)
-
-    const p = await yrp.computePackingPackageJson(UnitId('a'))
-    expect(p).toEqual({
-      name: 'a',
-      version: '1.0.0',
-      dependencies: {
-        boo: '200.1.0',
-        foo: '400.1.0',
-        goo: '100.1.0',
-      },
-    })
-  })
-
-  test('lorem ipsum', () => {
-    expect(5).toEqual(5)
   })
 })
