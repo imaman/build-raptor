3370e3b0cc8fe9c563c5d030fddf9ee80f96f31b
Itay Maman <94941+imaman@users.noreply.github.com>
2022-05-09 18:01:46 +0300

    passing

--- modules/yarn-repo-protocol/tests/b.spec.ts
+++ modules/yarn-repo-protocol/tests/b.spec.ts
@@ -1,11 +1,15 @@
 import { Driver } from 'build-raptor-core-testkit'
+import { createNopLogger } from 'logger'
+
+import { YarnRepoProtocol } from '../src'
 
 jest.setTimeout(30000)
 describe('b', () => {
+  const logger = createNopLogger()
   const testName = () => expect.getState().currentTestName
 
-  test('runs the build and test tasks of a package and captures their output', async () => {
-    const driver = new Driver(testName())
+  test('foo', async () => {
+    const driver = new Driver(testName(), { repoProtocol: new YarnRepoProtocol(logger) })
     const recipe = {
       'package.json': {
         name: 'foo',
@@ -16,16 +20,16 @@ describe('b', () => {
         name: 'a',
         version: '1.0.0',
         scripts: {
-          build: 'echo "building now"',
-          test: 'echo "testing now"',
+          build: 'mkdir -p dist/src dist/tests && echo "building now" && touch dist/src/a.js dist/tests/a.spec.js',
+          jest: `echo "testing now" && echo '{}' > jest-output.json`,
         },
       },
     }
 
     const fork = await driver.repo(recipe).fork()
 
-    const run = await fork.run('OK')
-    expect(await run.outputOf('build', 'a')).toEqual(['building now'])
-    expect(await run.outputOf('test', 'a')).toEqual(['testing now'])
+    const run = await fork.run('OK', { taskKind: 'test' })
+    expect(await run.outputOf('build', 'a')).toContain('building now')
+    expect(await run.outputOf('test', 'a')).toContain('testing now')
   })
 })
