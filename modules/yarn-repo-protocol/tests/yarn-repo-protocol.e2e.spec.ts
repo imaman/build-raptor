dfbf310809cb493637f2255f86f2d0816f20bc54
Itay Maman <94941+imaman@users.noreply.github.com>
2022-05-10 10:09:11 +0300

    test

--- modules/yarn-repo-protocol/tests/yarn-repo-protocol.e2e.spec.ts
+++ modules/yarn-repo-protocol/tests/yarn-repo-protocol.e2e.spec.ts
@@ -11,11 +11,7 @@ describe('yarn-repo-protocol.e2e', () => {
   test('runs tasks and captures their output', async () => {
     const driver = new Driver(testName(), { repoProtocol: new YarnRepoProtocol(logger) })
     const recipe = {
-      'package.json': {
-        name: 'foo',
-        private: true,
-        workspaces: ['modules/*'],
-      },
+      'package.json': { name: 'foo', private: true, workspaces: ['modules/*'] },
       'modules/a/package.json': {
         name: 'a',
         version: '1.0.0',
@@ -31,5 +27,42 @@ describe('yarn-repo-protocol.e2e', () => {
     const run = await fork.run('OK', { taskKind: 'test' })
     expect(await run.outputOf('build', 'a')).toContain('building now')
     expect(await run.outputOf('test', 'a')).toContain('testing now')
+  })
+
+  const build = [
+    'mkdir -p dist/src dist/tests',
+    `cat src/a.ts | tr '[:upper:]' '[:lower:]' > dist/src/a.js`,
+    `cat tests/a.spec.ts | tr '[:upper:]' '[:lower:]' > dist/tests/a.spec.js`,
+    `echo "build finished"`,
+  ].join(' && ')
+
+  const jest = [`cat dist/src/a.js dist/tests/a.spec.js`, `echo '{}' > jest-output.json`].join(' && ')
+
+  test('reruns tests when the source code changes', async () => {
+    const driver = new Driver(testName(), { repoProtocol: new YarnRepoProtocol(logger) })
+    const recipe = {
+      'package.json': { name: 'foo', private: true, workspaces: ['modules/*'] },
+      'modules/a/package.json': { name: 'a', version: '1.0.0', scripts: { build, jest } },
+      'modules/a/src/a.ts': 'N/A',
+      'modules/a/tests/a.spec.ts': 'TUESDAY',
+    }
+
+    const fork = await driver.repo(recipe).fork()
+
+    await fork.file('modules/a/src/a.ts').write('SUNDAY')
+    const runA = await fork.run('OK', { taskKind: 'test' })
+    expect(await fork.file('modules/a/dist/src/a.js').lines({ trimEach: true })).toEqual(['sunday'])
+    expect(await fork.file('modules/a/dist/tests/a.spec.js').lines({ trimEach: true })).toEqual(['tuesday'])
+    expect(runA.getSummary('a', 'build')).toMatchObject({ execution: 'EXECUTED' })
+    expect(runA.getSummary('a', 'test')).toMatchObject({ execution: 'EXECUTED' })
+    expect(await runA.outputOf('test', 'a')).toContain('sundaytuesday')
+
+    await fork.file('modules/a/src/a.ts').write('MONDAY')
+    const runB = await fork.run('OK', { taskKind: 'test' })
+    expect(await fork.file('modules/a/dist/src/a.js').lines({ trimEach: true })).toEqual(['monday'])
+    expect(await fork.file('modules/a/dist/tests/a.spec.js').lines({ trimEach: true })).toEqual(['tuesday'])
+    expect(runA.getSummary('a', 'build')).toMatchObject({ execution: 'EXECUTED' })
+    expect(runB.getSummary('a', 'test')).toMatchObject({ execution: 'EXECUTED' })
+    expect(await runB.outputOf('test', 'a')).toContain('mondaytuesday')
   })
 })
