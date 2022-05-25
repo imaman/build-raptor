7400ef4587a87e3c3cbebc62fc4edeab86d58a2a
Itay Maman <94941+imaman@users.noreply.github.com>
2022-05-25 17:49:40 +0300

    TypedPublisher: introducing .once() and .awaitFor() (#25)
    * once
    
    * awaitfor
    
    * _

--- modules/misc/src/typed-publisher.ts
+++ modules/misc/src/typed-publisher.ts
@@ -21,6 +21,7 @@ export interface Subscribable<T> {
 export class TypedPublisher<T extends Record<string, any>> implements Subscribable<T> {
   private readonly map: {
     [K in keyof T]: SinglePublisher<T[K]>
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
   } = {} as {
     [K in keyof T]: SinglePublisher<T[K]>
   }
@@ -36,5 +37,41 @@ export class TypedPublisher<T extends Record<string, any>> implements Subscribab
       this.map[k] = x
     }
     x.on(subscriber)
+  }
+
+  once<K extends keyof T>(k: K, subscriber: (e: T[K]) => Promise<void> | void) {
+    let fired = false
+    this.on(k, e => {
+      if (fired) {
+        return
+      }
+
+      fired = true
+      subscriber(e)
+    })
+  }
+
+  /**
+   * returns a promise that is resolved once an event the satisfies the given predicate has been published
+   * @param k event name
+   * @param predicate a function to test the event.
+   * @returns a Promise that resolves with the value of the first event for which `predicate` returned `true`
+   */
+  awaitFor<K extends keyof T>(k: K, predicate: (e: T[K]) => boolean): Promise<T[K]> {
+    return new Promise<T[K]>(res => {
+      let fired = false
+      this.on(k, e => {
+        if (fired) {
+          return
+        }
+
+        if (!predicate(e)) {
+          return
+        }
+
+        fired = true
+        res(e)
+      })
+    })
   }
 }
