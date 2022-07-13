67c969f7394565aa8af381bc7a41918945ddafa5
Itay Maman <94941+imaman@users.noreply.github.com>
2022-07-13 13:51:45 +0300

    implement s3 storage client

--- modules/s3-storage-client/src/s3-storage-client.ts
+++ modules/s3-storage-client/src/s3-storage-client.ts
@@ -1,1 +1,75 @@
-export class S3StorageClient implements StorageClient {}
+import { S3 } from '@aws-sdk/client-s3'
+import { computeObjectHash, Key, shouldNeverHappen, StorageClient } from 'misc'
+import { Stream } from 'stream'
+import * as StreamConsumers from 'stream/consumers'
+import * as util from 'util'
+
+export class S3StorageClient implements StorageClient {
+  private readonly s3 = new S3({})
+
+  constructor(private readonly bucketName: string, private readonly pathPrefix: string) {
+    if (pathPrefix.endsWith('/')) {
+      throw new Error(`Illegal path prefix value`)
+    }
+
+    if (pathPrefix.match(/^[a-zA-Z0-9][a-zA-Z0-9_-/]*$/)) {
+      throw new Error(`path prefix value is invalid`)
+    }
+
+    if (pathPrefix.includes('//')) {
+      throw new Error(`path prefix value cannot include two consecutive slashes`)
+    }
+  }
+
+  private resolvePath(key: Key): string {
+    return `${this.pathPrefix}/${computeObjectHash({ key })}`
+  }
+
+  async putObject(key: Key, content: string | Buffer): Promise<void> {
+    await this.s3.putObject({ Bucket: this.bucketName, Key: this.resolvePath(key), Body: content })
+    return
+  }
+
+  getObject(key: Key): Promise<string>
+  getObject(key: Key, type: 'string'): Promise<string>
+  getObject(key: Key, type: 'buffer'): Promise<Buffer>
+  async getObject(key: Key, type?: 'string' | 'buffer'): Promise<string | Buffer> {
+    let resp
+    try {
+      resp = await this.s3.getObject({ Bucket: this.bucketName, Key: this.resolvePath(key) })
+    } catch (e) {
+      // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+      const typed = e as { message?: string }
+      throw new Error(`Failed to read an object from S3: ${typed.message ?? util.inspect(e)}`)
+    }
+
+    if (!resp.Body) {
+      throw new Error(`Body is falsy`)
+    }
+
+    if (!(resp.Body instanceof Stream)) {
+      throw new Error(`unsupported type of body (type: ${typeof resp.Body})`)
+    }
+
+    const buffer = await StreamConsumers.buffer(resp.Body)
+
+    if (type === 'buffer') {
+      return buffer
+    }
+
+    if (type === 'string' || type === undefined) {
+      return buffer.toString('utf-8')
+    }
+
+    shouldNeverHappen(type)
+  }
+
+  async objectExists(key: Key): Promise<boolean> {
+    try {
+      await this.s3.headObject({ Bucket: this.bucketName, Key: this.resolvePath(key) })
+      return true
+    } catch (e) {
+      return false
+    }
+  }
+}
