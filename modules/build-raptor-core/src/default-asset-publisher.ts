e685e6f3acc7c308763dc40c56705b865061acd6
Itay Maman <94941+imaman@users.noreply.github.com>
2022-07-21 21:26:03 +0300

    implement it

--- modules/build-raptor-core/src/default-asset-publisher.ts
+++ modules/build-raptor-core/src/default-asset-publisher.ts
@@ -1,11 +1,18 @@
-import { StorageClient } from 'misc'
+import { Logger } from 'logger'
+import { computeHash, StorageClient } from 'misc'
 import { Publisher } from 'repo-protocol'
 import { UnitMetadata } from 'unit-metadata'
 
 export class DefaultAssetPublisher implements Publisher {
-  constructor(private readonly storageClient: StorageClient) {}
+  constructor(private readonly storageClient: StorageClient, private readonly logger: Logger) {}
 
-  publishAsset(_u: UnitMetadata, _content: Buffer, _name: string): Promise<void> {
-    throw new Error('Method not implemented.')
+  async publishAsset(u: UnitMetadata, content: Buffer, name: string): Promise<void> {
+    const fingerprint = computeHash(content)
+    const resolved = this.storageClient.putObject({ fingerprint }, content)
+    if (typeof resolved !== 'string') {
+      throw new Error(`Bad type returned from put-object: ${typeof resolved}`)
+    }
+
+    this.logger.info(`Asset ${u.id}/${name} uploaded to ${resolved}`)
   }
 }
