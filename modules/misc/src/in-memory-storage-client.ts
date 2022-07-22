71fe43ec1580d32bc6e49e88937a4daed9eb48fd
Itay Maman <94941+imaman@users.noreply.github.com>
2022-07-22 16:43:49 +0300

    implment: in-memory-s-c

--- modules/misc/src/in-memory-storage-client.ts
+++ modules/misc/src/in-memory-storage-client.ts
@@ -1,22 +1,33 @@
 import { Int } from '.'
 import { shouldNeverHappen } from './constructs'
-import { computeObjectHash } from './misc'
+import { computeHash, computeObjectHash } from './misc'
 import { Key, StorageClient } from './storage-client'
 
 export class InMemoryStorageClient implements StorageClient {
   private byteCount_: Int = Int(0)
   constructor(private readonly sizeLimitInBytes: Int = Int(-1)) {}
+
   private readonly store = new Map<Key, string>()
 
-  async putObject(key: Key, content: string): Promise<void> {
-    const s = this.keyToString(key)
+  async putObject(key: Key, content: string | Buffer): Promise<void> {
+    this.putObjectImpl(this.keyToString(key), content)
+  }
+
+  private putObjectImpl(s: string, content: string | Buffer) {
     const existing = this.store.get(s) ?? ''
     const newCount = this.byteCount_ + Buffer.from(content).length - Buffer.from(existing).length
     if (this.sizeLimitInBytes >= 0 && newCount > this.sizeLimitInBytes) {
       throw new Error(`size limit (${this.sizeLimitInBytes} bytes) will be exceeded.`)
     }
-    this.store.set(s, content)
+
+    this.store.set(s, typeof content === 'string' ? content : content.toString('utf-8'))
     this.byteCount_ = Int(newCount)
+  }
+
+  async putContentAddressable(content: string | Buffer): Promise<string> {
+    const ret = `cas/${computeHash(content)}`
+    this.putObjectImpl(ret, content)
+    return ret
   }
 
   get byteCount() {
@@ -40,6 +51,6 @@ export class InMemoryStorageClient implements StorageClient {
   }
 
   private keyToString(key: Key): string {
-    return computeObjectHash({ key })
+    return `std/${computeObjectHash({ key })}`
   }
 }
