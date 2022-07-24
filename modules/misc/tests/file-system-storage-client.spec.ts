7efc275915f26a179d5d1c2ab6b8f24694a3dc49
Itay Maman <94941+imaman@users.noreply.github.com>
2022-07-24 11:34:43 +0300

    _

--- modules/misc/tests/file-system-storage-client.spec.ts
+++ modules/misc/tests/file-system-storage-client.spec.ts
@@ -1,6 +1,6 @@
 import { FilesystemStorageClient } from '../src/file-system-storage-client'
 import { folderify } from '../src/folderify'
-import { storageClientContract } from './storage-client-contract-test'
+import { storageClientContract } from './storage-client-contract'
 
 describe('file-system-storage-client', () => {
   storageClientContract(async () => await FilesystemStorageClient.create(await folderify({})))
