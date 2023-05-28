fa96a362dca9809ea8c25515db5e729a62dfadf0
raphaelbharel <53297838+raphaelbharel@users.noreply.github.com>
2023-05-28 21:28:40 +0300

    more specific zod

--- modules/build-raptor/src/build-tracker-api.ts
+++ modules/build-raptor/src/build-tracker-api.ts
@@ -3,6 +3,6 @@ import { z } from 'zod'
 export const RegisterAssetRequest = z.object({
   packageName: z.string(),
   commitHash: z.string(),
-  prNumber: z.number().optional(),
+  prNumber: z.number().int().positive().optional(),
   casReference: z.string(),
 })
