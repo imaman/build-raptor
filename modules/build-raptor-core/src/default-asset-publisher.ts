06ba5debdf9a367c1eed8c925464aafc71046f08
Itay Maman <94941+imaman@users.noreply.github.com>
2023-05-31 20:01:32 +0300

    subscribe

--- modules/build-raptor-core/src/default-asset-publisher.ts
+++ modules/build-raptor-core/src/default-asset-publisher.ts
@@ -4,16 +4,11 @@ import { Publisher } from 'repo-protocol'
 import { UnitMetadata } from 'unit-metadata'
 
 export class DefaultAssetPublisher implements Publisher {
-  constructor(
-    private readonly storageClient: StorageClient,
-    private readonly logger: Logger,
-    private readonly callback: (u: UnitMetadata, resolved: string) => Promise<void>,
-  ) {}
+  constructor(private readonly storageClient: StorageClient, private readonly logger: Logger) {}
 
   async publishAsset(u: UnitMetadata, content: Buffer, name: string): Promise<string> {
     const resolved = await this.storageClient.putContentAddressable(content)
     this.logger.info(`Asset ${u.id}/${name} uploaded to CAS ${resolved}`)
-    await this.callback(u, resolved)
     return resolved
   }
 }
