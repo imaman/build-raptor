17fb23f12962e3fc64a8b2ef9d433af601acf245
Itay Maman <94941+imaman@users.noreply.github.com>
2023-06-06 17:54:24 +0300

    cleanup

--- modules/misc/tests/file-system-storage-client.spec.ts
+++ modules/misc/tests/file-system-storage-client.spec.ts
@@ -1,7 +1,27 @@
+import { chaoticDeterministicString, slurpDir } from '../src'
 import { FilesystemStorageClient } from '../src/file-system-storage-client'
 import { folderify } from '../src/folderify'
 import { storageClientContract } from './storage-client-contract'
 
 describe('file-system-storage-client', () => {
   storageClientContract(async () => await FilesystemStorageClient.create(await folderify({})))
+
+  describe('cleanup', () => {
+    test('when the options is specified cleans up files if total size exceeds the given threshold', async () => {
+      const dir = await folderify({})
+
+      const fsc = await FilesystemStorageClient.create(dir)
+      expect(Object.keys(await slurpDir(dir))).toHaveLength(0)
+      await fsc.putObject('a', chaoticDeterministicString(100, '_'))
+      await fsc.putObject('b', chaoticDeterministicString(100, '_'))
+      await fsc.putObject('c', chaoticDeterministicString(100, '_'))
+      expect(Object.keys(await slurpDir(dir))).toHaveLength(3)
+
+      await FilesystemStorageClient.create(dir, { triggerCleanupIfByteSizeExceeds: 300 })
+      expect(Object.keys(await slurpDir(dir))).toHaveLength(3)
+
+      await FilesystemStorageClient.create(dir, { triggerCleanupIfByteSizeExceeds: 299 })
+      expect(Object.keys(await slurpDir(dir))).toHaveLength(2)
+    })
+  })
 })
