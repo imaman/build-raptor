98109dfce4143c96140128e0586be78ff985b959
Itay Maman <94941+imaman@users.noreply.github.com>
2023-06-13 19:09:42 +0300

    dynamic loading

--- modules/build-raptor-core/src/step-by-step-transmitter.ts
+++ modules/build-raptor-core/src/step-by-step-transmitter.ts
@@ -1,11 +1,15 @@
-import { Step, StepByStep } from 'build-raptor-api'
+import { Step, StepByStep, StepByStepProcessor } from 'build-raptor-api'
 import * as fs from 'fs'
 import { Logger } from 'logger'
 
 export class StepByStepTransmitter {
   private readonly steps: Step[] = []
 
-  constructor(private readonly stepByStepFile: string, private readonly logger: Logger) {}
+  private constructor(
+    private readonly stepByStepFile: string,
+    private readonly stepByStepProcessor: StepByStepProcessor | undefined,
+    private readonly logger: Logger,
+  ) {}
 
   push(step: Step) {
     const parsed = Step.parse(step)
@@ -16,5 +20,20 @@ export class StepByStepTransmitter {
     const parsed = StepByStep.parse(this.steps)
     fs.writeFileSync(this.stepByStepFile, JSON.stringify(parsed))
     this.logger.info(`step by step written to ${this.stepByStepFile}`)
+  }
+
+  static async create(stepByStepFile: string, stepByStepProcessorModuleName: string | undefined, logger: Logger) {
+    let processor
+    if (stepByStepProcessorModuleName) {
+      const imported = await import(stepByStepProcessorModuleName)
+      processor = imported.default
+      if (!(processor instanceof StepByStepProcessor)) {
+        throw new Error(
+          `object loaded from ${stepByStepProcessorModuleName} is not an instance of ${StepByStepProcessor.name}`,
+        )
+      }
+    }
+
+    return new StepByStepTransmitter(stepByStepFile, processor, logger)
   }
 }
