aaddffad3f4fcd4bc66056a469df46db77f94d52
Itay Maman <94941+imaman@users.noreply.github.com>
2023-07-20 15:38:55 +0300

    coretypes

--- modules/core-types/src/core-types.ts
+++ modules/core-types/src/core-types.ts
@@ -1,7 +1,35 @@
 import { Brand } from 'brand'
+import { threeWaySplit } from 'misc'
+import * as path from 'path'
 
-export type PathInRepo = Brand<string, 'PathInRepo'>
+export type RepoRoot = Brand<string, 'RepoRoot'>
+
+export function RepoRoot(input: string) {
+  return {
+    resolve: (pathInRepo: PathInRepo) => path.join(input, pathInRepo.val),
+  }
+}
+
+type Mark = Brand<string, 'PathInRepo'>
+
+export type PathInRepo = {
+  readonly mark: Mark
+  val: string
+  toJSON: () => string
+}
 
 export function PathInRepo(input: string): PathInRepo {
-  return input as PathInRepo // eslint-disable-line @typescript-eslint/consistent-type-assertions
+  const val = norm(input)
+  return {
+    mark: '' as Mark, // eslint-disable-line @typescript-eslint/consistent-type-assertions
+    val,
+    toJSON: () => val,
+  }
 }
+
+const norm = (s: string) =>
+  threeWaySplit(
+    path.normalize(s),
+    () => false,
+    c => c === '/',
+  ).mid
