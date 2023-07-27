f5bafe970b15d04aed573a111392e71772d315b1
Itay Maman <94941+imaman@users.noreply.github.com>
2023-07-28 02:17:28 +0300

    reorder

--- modules/repo-protocol/src/task-info.ts
+++ modules/repo-protocol/src/task-info.ts
@@ -5,7 +5,7 @@ export type OutputLocation = { pathInRepo: PathInRepo; purge: 'ALWAYS' | 'NEVER'
 
 export type TaskInfo = {
   readonly taskName: TaskName
-  readonly deps?: TaskName[]
-  readonly outputLocations?: OutputLocation[]
   readonly inputs?: PathInRepo[]
+  readonly outputLocations?: OutputLocation[]
+  readonly deps?: TaskName[]
 }
