e02e9b561313069a2a273ae6a7c22c11dace6046
Itay Maman <94941+imaman@users.noreply.github.com>
2023-07-31 14:10:28 +0300

    print

--- modules/build-raptor-core/src/tar-stream.ts
+++ modules/build-raptor-core/src/tar-stream.ts
@@ -1,21 +1,7 @@
-import { Brand } from 'brand'
 import * as child_process from 'child_process'
-import { PathInRepo, RepoRoot } from 'core-types'
 import * as fs from 'fs'
-import { createWriteStream } from 'fs'
-import * as fse from 'fs-extra'
-import { Logger } from 'logger'
-import { computeHash, computeObjectHash, DirectoryScanner, Key, promises, StorageClient, TypedPublisher } from 'misc'
 import * as path from 'path'
-import * as stream from 'stream'
-import { TaskName } from 'task-name'
-import * as Tmp from 'tmp-promise'
-import * as util from 'util'
-import * as zlib from 'zlib'
 import { z } from 'zod'
-
-import { Fingerprint } from './fingerprint'
-import { TaskStoreEvent } from './task-store-event'
 
 const Info = z.object({
   path: z.string(),
@@ -29,7 +15,6 @@ interface Entry {
   content: Buffer
   info: Info
 }
-
 
 // TOOD(imaman): move to its own file + rename
 export class TarStream {
@@ -51,7 +36,7 @@ export class TarStream {
   toBuffer() {
     let sum = 0
     for (const entry of this.entires) {
-      console.log(`++entry.info.mtime=${JSON.stringify(entry.info.mtime)}`)
+      print(`++entry.info.mtime=${JSON.stringify(entry.info.mtime)}`)
       const b = Buffer.from(JSON.stringify(Info.parse(entry.info)))
       sum += 4 + b.length + entry.content.length
     }
@@ -100,12 +85,12 @@ export class TarStream {
       offset = contentEndOffset
 
       const resolved = resolve(parsedInfo.path)
-      fs.mkdirSync(path.dirname(resolved), { recursive: true  })
+      fs.mkdirSync(path.dirname(resolved), { recursive: true })
       fs.writeFileSync(resolved, contentBuf, { mode: parsedInfo.mode })
 
       const RATIO = 1000000n
       const ns = BigInt(parsedInfo.mtime)
-      console.log(`parsedInfo=${JSON.stringify(parsedInfo)}`)
+      print(`parsedInfo=${JSON.stringify(parsedInfo)}`)
       const d = new Date(Number(ns / RATIO))
       if (useTouch) {
         const ts = d.toISOString().slice(0, -1)
@@ -117,7 +102,7 @@ export class TarStream {
 
         // const m2 = fs.statSync(resolved).mtime
         // if (d.toISOString() !== new Date(m2).toISOString()){
-        //   console.log(`mismatch: ${d} vs. ${m2}`)
+        //   print(`mismatch: ${d} vs. ${m2}`)
         // }
       }
 
@@ -128,3 +113,7 @@ export class TarStream {
   }
 }
 export const useTouch = false
+
+function print(msg: string) {
+  console.log(msg) // eslint-disable-line no-console
+}
