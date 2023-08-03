bbce3e87e7ab29e5dcd5063d63cf700b91d7b42b
Itay Maman <94941+imaman@users.noreply.github.com>
2023-08-03 18:28:39 +0300

    offdormanton

--- modules/yarn-repo-protocol/src/yarn-repo-protocol-config.ts
+++ modules/yarn-repo-protocol/src/yarn-repo-protocol-config.ts
@@ -3,7 +3,10 @@ import { z } from 'zod'
 export const YarnRepoProtocolConfig = z
   .object({
     uberBuild: z.boolean().optional(),
-    install: z.boolean().optional(),
+    install: z
+      .boolean()
+      .optional()
+      .or(z.union([z.literal('off'), z.literal('dormant'), z.literal('on')])),
   })
   .strict()
 export type YarnRepoProtocolConfig = z.infer<typeof YarnRepoProtocolConfig>
