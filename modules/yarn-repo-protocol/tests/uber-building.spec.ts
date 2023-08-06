551f25ab58c3a3438a4382e88837590082af9926
Itay Maman <94941+imaman@users.noreply.github.com>
2023-08-06 21:50:30 +0300

    nest

--- modules/yarn-repo-protocol/tests/uber-building.spec.ts
+++ modules/yarn-repo-protocol/tests/uber-building.spec.ts
@@ -5,85 +5,88 @@ import { createNopLogger } from 'logger'
 import { YarnRepoProtocol } from '../src/yarn-repo-protocol'
 
 jest.setTimeout(120000)
-describe('uber-building', () => {
-  const logger = createNopLogger()
 
-  function newYarnRepoProtocol() {
-    return new YarnRepoProtocol(logger, new NopAssetPublisher())
-  }
-  const testName = () => expect.getState().currentTestName
-  test('the build output contains errors from all modules', async () => {
-    const driver = new Driver(testName(), { repoProtocol: newYarnRepoProtocol() })
-    const recipe = {
-      'package.json': { name: 'foo', private: true, workspaces: ['modules/*'] },
-      'modules/a/package.json': driver.packageJson('a', ['b']),
-      'modules/a/src/a.ts': `
+describe('b', () => {
+  describe('uber-building', () => {
+    const logger = createNopLogger()
+
+    function newYarnRepoProtocol() {
+      return new YarnRepoProtocol(logger, new NopAssetPublisher())
+    }
+    const testName = () => expect.getState().currentTestName
+    test('the build output contains errors from all modules', async () => {
+      const driver = new Driver(testName(), { repoProtocol: newYarnRepoProtocol() })
+      const recipe = {
+        'package.json': { name: 'foo', private: true, workspaces: ['modules/*'] },
+        'modules/a/package.json': driver.packageJson('a', ['b']),
+        'modules/a/src/a.ts': `
           import {b} from 'b'
           export function a(n: number) { return b(n)*10+1 }`,
-      'modules/a/tests/a.spec.ts': `import {a} from '../src/a';  test('a', () => { expect(a(0)).toEqual(-321) })`,
-      'modules/b/package.json': driver.packageJson('b', ['c']),
-      'modules/b/src/index.ts': `
+        'modules/a/tests/a.spec.ts': `import {a} from '../src/a';  test('a', () => { expect(a(0)).toEqual(-321) })`,
+        'modules/b/package.json': driver.packageJson('b', ['c']),
+        'modules/b/src/index.ts': `
           import {c} from 'c'
           export function b(n: number) { return c(n)*10+2 }`,
-      'modules/b/tests/b.spec.ts': `import {b} from '../src'; test('b', () => {expect(b(0)).toEqual(32)})`,
-      'modules/c/package.json': driver.packageJson('c'),
-      'modules/c/src/index.ts': `export function c(s: string) { return s.length }`,
-      'modules/c/tests/c.spec.ts': `import {c} from '../src'; test('xyz', () => {expect(c('a')).toEqual(3)})`,
-    }
+        'modules/b/tests/b.spec.ts': `import {b} from '../src'; test('b', () => {expect(b(0)).toEqual(32)})`,
+        'modules/c/package.json': driver.packageJson('c'),
+        'modules/c/src/index.ts': `export function c(s: string) { return s.length }`,
+        'modules/c/tests/c.spec.ts': `import {c} from '../src'; test('xyz', () => {expect(c('a')).toEqual(3)})`,
+      }
 
-    const fork = await driver.repo(recipe).fork()
+      const fork = await driver.repo(recipe).fork()
 
-    const run1 = await fork.run('FAIL', { taskKind: 'build' })
-    expect(await run1.outputOf('build', 'c')).toEqual([
-      `modules/b/src/index.ts(3,51): error TS2345: Argument of type 'number' is not assignable to parameter of type 'string'.`,
-    ])
+      const run1 = await fork.run('FAIL', { taskKind: 'build' })
+      expect(await run1.outputOf('build', 'c')).toEqual([
+        `modules/b/src/index.ts(3,51): error TS2345: Argument of type 'number' is not assignable to parameter of type 'string'.`,
+      ])
 
-    await fork.file('modules/c/src/index.ts').write(`export function c(n: number) { return n+3 }`)
-    await fork
-      .file('modules/c/tests/c.spec.ts')
-      .write(`import {c} from '../src'; test('xyz', () => {expect(c(90)).toEqual(93)})`)
-    const run2 = await fork.run('OK', { taskKind: 'build' })
-    expect(await run2.outputOf('build', 'c')).toEqual([``])
+      await fork.file('modules/c/src/index.ts').write(`export function c(n: number) { return n+3 }`)
+      await fork
+        .file('modules/c/tests/c.spec.ts')
+        .write(`import {c} from '../src'; test('xyz', () => {expect(c(90)).toEqual(93)})`)
+      const run2 = await fork.run('OK', { taskKind: 'build' })
+      expect(await run2.outputOf('build', 'c')).toEqual([``])
 
-    const run3 = await fork.run('FAIL', { taskKind: 'test' })
-    expect(await run3.outputOf('test', 'a')).toContain('    Received: 321')
+      const run3 = await fork.run('FAIL', { taskKind: 'test' })
+      expect(await run3.outputOf('test', 'a')).toContain('    Received: 321')
 
-    await fork
-      .file('modules/a/tests/a.spec.ts')
-      .write(`import {a} from '../src/a';  test('a', () => { expect(a(0)).toEqual(321) })`)
-    await fork.run('OK', { taskKind: 'test' })
-  })
-  test('runs build:post after compilation', async () => {
-    const driver = new Driver(testName(), { repoProtocol: newYarnRepoProtocol() })
-    const recipe = {
-      'package.json': { name: 'foo', private: true, workspaces: ['modules/*'] },
-      'modules/a/package.json': driver.packageJson('a', [], {
-        'build:post': `cat dist/src/a.js && echo "brown fox"`,
-      }),
-      'modules/a/src/a.ts': `export const a = "the quick"`,
-      'modules/a/tests/a.spec.ts': ``,
-    }
+      await fork
+        .file('modules/a/tests/a.spec.ts')
+        .write(`import {a} from '../src/a';  test('a', () => { expect(a(0)).toEqual(321) })`)
+      await fork.run('OK', { taskKind: 'test' })
+    })
+    test('runs build:post after compilation', async () => {
+      const driver = new Driver(testName(), { repoProtocol: newYarnRepoProtocol() })
+      const recipe = {
+        'package.json': { name: 'foo', private: true, workspaces: ['modules/*'] },
+        'modules/a/package.json': driver.packageJson('a', [], {
+          'build:post': `cat dist/src/a.js && echo "brown fox"`,
+        }),
+        'modules/a/src/a.ts': `export const a = "the quick"`,
+        'modules/a/tests/a.spec.ts': ``,
+      }
 
-    const fork = await driver.repo(recipe).fork()
-    const run1 = await fork.run('OK', { taskKind: 'build' })
-    const output = await run1.outputOf('build', 'a')
-    expect(output.join('\n')).toContain('the quick')
-    expect(output[output.length - 1]).toMatch(/brown fox$/)
-  })
-  test('fails the task if build:post failed', async () => {
-    const driver = new Driver(testName(), { repoProtocol: newYarnRepoProtocol() })
-    const recipe = {
-      'package.json': { name: 'foo', private: true, workspaces: ['modules/*'] },
-      'modules/a/package.json': driver.packageJson('a', [], { 'build:post': `cat foo/boo/non-existing-file` }),
-      'modules/a/src/a.ts': `export const a = "the quick"`,
-      'modules/a/tests/a.spec.ts': ``,
-    }
+      const fork = await driver.repo(recipe).fork()
+      const run1 = await fork.run('OK', { taskKind: 'build' })
+      const output = await run1.outputOf('build', 'a')
+      expect(output.join('\n')).toContain('the quick')
+      expect(output[output.length - 1]).toMatch(/brown fox$/)
+    })
+    test('fails the task if build:post failed', async () => {
+      const driver = new Driver(testName(), { repoProtocol: newYarnRepoProtocol() })
+      const recipe = {
+        'package.json': { name: 'foo', private: true, workspaces: ['modules/*'] },
+        'modules/a/package.json': driver.packageJson('a', [], { 'build:post': `cat foo/boo/non-existing-file` }),
+        'modules/a/src/a.ts': `export const a = "the quick"`,
+        'modules/a/tests/a.spec.ts': ``,
+      }
 
-    const fork = await driver.repo(recipe).fork()
-    const run1 = await fork.run('FAIL', { taskKind: 'build' })
-    expect(await run1.outputOf('build', 'a')).toContainEqual(
-      expect.stringMatching('foo/boo/non-existing-file: No such file or directory'),
-    )
+      const fork = await driver.repo(recipe).fork()
+      const run1 = await fork.run('FAIL', { taskKind: 'build' })
+      expect(await run1.outputOf('build', 'a')).toContainEqual(
+        expect.stringMatching('foo/boo/non-existing-file: No such file or directory'),
+      )
+    })
   })
   describe('deletion', () => {
     const logger = createNopLogger()
