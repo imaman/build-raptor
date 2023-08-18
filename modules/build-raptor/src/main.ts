f7803c1a65fbad492b95387fd7be229681a526c0
Itay Maman <94941+imaman@users.noreply.github.com>
2023-08-19 00:35:42 +0300

    reorder

--- modules/build-raptor/src/main.ts
+++ modules/build-raptor/src/main.ts
@@ -1,8 +1,9 @@
 #!/usr/bin/env -S node --enable-source-maps
-import { main } from './build-raptor-cli'
 
-// Needed for import-time symlinking
+// Needed for import-time symlinking. Must appear before every other import.
 // TODO(imaman): find a solution that is transparent for the user code.
 export * from './index'
+
+import { main } from './build-raptor-cli'
 
 main()
