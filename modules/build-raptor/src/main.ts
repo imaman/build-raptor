b9a4accee6448ca21e0e76a9ccf0f4831154edbb
Itay Maman <94941+imaman@users.noreply.github.com>
2023-08-19 00:32:06 +0300

    import index

--- modules/build-raptor/src/main.ts
+++ modules/build-raptor/src/main.ts
@@ -1,4 +1,8 @@
 #!/usr/bin/env -S node --enable-source-maps
 import { main } from './build-raptor-cli'
 
+// Needed for import-time symlinking
+// TODO(imaman): find a solution that is transparent for the user code.
+export * from './index'
+
 main()
