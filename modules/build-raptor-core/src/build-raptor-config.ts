6d90d89b0be4b959d78e009dcf2ae3d48843660a
Itay Maman <94941+imaman@users.noreply.github.com>
2023-08-19 23:03:09 +0300

    config

--- modules/build-raptor-core/src/build-raptor-config.ts
+++ modules/build-raptor-core/src/build-raptor-config.ts
@@ -1,11 +1,21 @@
 import { z } from 'zod'
 
 export const BuildRaptorConfig = z.object({
+  /**
+   * Repo-protocol-specific configuration.
+   */
   repoProtocol: z.unknown().optional(),
   /**
    * task names for which high-detail printing will be enabled. This allow the user to get detailed information about
    * specific tasks. Example: ['foo:build', 'bar:test'].
    */
   verbosePrintTasks: z.string().array().optional(),
+  /**
+   * Selects one of two modes for determining when a task will run. A task will always run if one of the tasks listed in
+   * its TaskInfo.deps had to run.
+   * Additionally, a task will run if any of its inputs have changed (when this attribute is true) or if any of the
+   * tasks that generates its inputs had to run (when this attribute is false).
+   */
+  tightFingerprints: z.boolean().optional(),
 })
 export type BuildRaptorConfig = z.infer<typeof BuildRaptorConfig>
