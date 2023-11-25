0c89bcf3f41f9bc668e495f8bf375faca31b04bf
Itay Maman <94941+imaman@users.noreply.github.com>
2023-11-25 08:37:01 +0200

    deplisttest

--- modules/yarn-repo-protocol/src/yarn-repo-protocol.ts
+++ modules/yarn-repo-protocol/src/yarn-repo-protocol.ts
@@ -808,6 +808,7 @@ export class YarnRepoProtocol implements RepoProtocol {
       taskName: TaskName(u.id, TaskKind('publish-assets')),
       outputLocations: [{ pathInRepo: dir.expand(PREPARED_ASSETS_DIR), purge: 'NEVER' }],
       inputs: [dir.expand('package.json'), dir.expand(this.dist('s'))],
+      deps: this.depList(TaskName(u.id, TaskKind('test'))),
     }
   }
 
