9644401456942383719726ab72b139fb57814d79
Itay Maman <94941+imaman@users.noreply.github.com>
2023-12-16 07:50:51 +0200

    lookup

--- modules/build-raptor-core/src/execution-plan.ts
+++ modules/build-raptor-core/src/execution-plan.ts
@@ -1,14 +1,22 @@
+import { BuildFailedError } from 'build-failed-error'
+import { PathInRepo } from 'core-types'
 import { Logger } from 'logger'
 import { Graph, hardGet } from 'misc'
 import { TaskName } from 'task-name'
 
 import { Task } from './task'
+import { TaskOutputRegistry } from './validate-task-infos'
 
 // TODO(imaman): rethink the name. It is not exactly a "Plan".
 export class ExecutionPlan {
   private readonly taskByName = new Map<TaskName, Task>()
   readonly errorPropagationGraph: Graph<TaskName>
-  constructor(readonly taskGraph: Graph<TaskName>, tasks: Task[], private readonly logger: Logger) {
+  constructor(
+    readonly taskGraph: Graph<TaskName>,
+    tasks: Task[],
+    private readonly logger: Logger,
+    private readonly registry: TaskOutputRegistry,
+  ) {
     for (const t of tasks) {
       this.taskByName.set(t.name, t)
     }
@@ -25,8 +33,8 @@ export class ExecutionPlan {
     return taskNames.map(tn => this.getTask(tn))
   }
 
-  apply(commands: string[], units: string[]) {
-    const startingPoints = this.computeStartingPoints(commands, units)
+  apply(commands: string[], units: string[], outputLocations: PathInRepo[]) {
+    const startingPoints = this.computeStartingPoints(commands, units, outputLocations)
     this.dropOutOfScope(startingPoints)
     return startingPoints
   }
@@ -42,16 +50,25 @@ export class ExecutionPlan {
     this.logger.info(`Task graph (only in-scope):\n${this.taskGraph}`)
   }
 
-  private computeStartingPoints(commands: string[], units: string[]) {
+  private computeStartingPoints(commands: string[], units: string[], outputLocations: PathInRepo[]) {
     const setOfUnitId = new Set<string>(units)
     this.logger.info(`setOfUnitId=${[...setOfUnitId].join('; ')}`)
     this.logger.info(`command=<${commands}>`)
+    const ret = outputLocations.map(ol => {
+      const tn = this.registry.lookup(ol)
+      if (!tn) {
+        throw new BuildFailedError(`no task found for this output location: ${ol}`)
+      }
+      return tn
+    })
     const matchesUnit = setOfUnitId.size === 0 ? () => true : (t: Task) => setOfUnitId.has(t.unitId)
     const matchesCommand = commands.length === 0 ? () => true : (t: Task) => commands.includes(t.kind)
-    const ret = this.tasks()
-      .filter(t => matchesUnit(t) && matchesCommand(t))
-      .map(t => t.name)
-      .sort()
+    ret.push(
+      ...this.tasks()
+        .filter(t => matchesUnit(t) && matchesCommand(t))
+        .map(t => t.name),
+    )
+    ret.sort()
 
     this.logger.info(`Found ${ret.length} starting points`)
     return ret
