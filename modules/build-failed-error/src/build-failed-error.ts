37c6bca7c7562704da88bcb3bbe5251094961cbe
Itay Maman <94941+imaman@users.noreply.github.com>
2023-12-20 20:57:05 +0200

    doc

--- modules/build-failed-error/src/build-failed-error.ts
+++ modules/build-failed-error/src/build-failed-error.ts
@@ -1,5 +1,15 @@
+type HintType =
+  /**
+   * Failure while running a build task (including, for instance, test tasks)
+   */
+  | 'task'
+  /**
+   * Failure while running a program (due to `build-raptor run` command)
+   */
+  | 'program'
+
 export class BuildFailedError extends Error {
-  constructor(m: string, readonly hint: 'task' | 'program' = 'task') {
+  constructor(m: string, readonly hint: HintType = 'task') {
     super(m)
 
     // Set the prototype explicitly.
