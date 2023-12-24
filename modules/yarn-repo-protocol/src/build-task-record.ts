eac280f0a5140085875166c7c7c1d0f62e5043d4
Itay Maman <94941+imaman@users.noreply.github.com>
2023-12-24 17:05:06 +0200

    orstring

--- modules/yarn-repo-protocol/src/build-task-record.ts
+++ modules/yarn-repo-protocol/src/build-task-record.ts
@@ -3,9 +3,9 @@ import { z } from 'zod'
 export const BuildTaskRecord = z.record(
   z.string(),
   z.object({
-    inputs: z.string().array(),
-    outputs: z.string().array(),
-    labels: z.string().array().optional(),
+    inputs: z.string().array().or(z.string()),
+    outputs: z.string().array().or(z.string()),
+    labels: z.string().array().or(z.string()).optional(),
   }),
 )
 export type BuildTaskRecord = z.infer<typeof BuildTaskRecord>
