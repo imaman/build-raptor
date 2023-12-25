75d3e26c42eebaff60ab91cda8d3f3f91ffe888a
Itay Maman <94941+imaman@users.noreply.github.com>
2023-12-25 09:41:44 +0200

    compiles

--- modules/build-raptor-core/src/planner.ts
+++ modules/build-raptor-core/src/planner.ts
@@ -7,7 +7,8 @@ import { TaskName } from 'task-name'
 import { ExecutionPlan } from './execution-plan'
 import { Model } from './model'
 import { Task } from './task'
-import { TaskOutputRegistry, validateTaskInfos } from './validate-task-infos'
+import { TaskOutputRegistry } from './task-output-registry'
+import { validateTaskInfos } from './validate-task-infos'
 
 export class Planner {
   private readonly taskGraph = new Graph<TaskName>(t => t)
