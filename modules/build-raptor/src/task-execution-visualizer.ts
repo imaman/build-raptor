615daa883e369012af4d94f59e57115e2eb1c89b
Itay Maman <94941+imaman@users.noreply.github.com>
2025-01-08 23:34:24 +0200

    simple vis

--- modules/build-raptor/src/task-execution-visualizer.ts
+++ modules/build-raptor/src/task-execution-visualizer.ts
@@ -1,62 +1,23 @@
 export class TaskExecutionVisualizer {
-  private readonly runningTasks = new Map<string, number>() // taskName -> indentation level
-  private readonly INDENTATION_STEP = '    '
+  private numStarted = 0
+  private numEnded = 0
 
   private getLine(taskName: string, text?: string): string {
-    const maxIndent = this.activeIndents().at(-1)
-    if (maxIndent === undefined) {
-      return ''
+    if (text === undefined) {
+      return `[${this.numEnded}/${this.numStarted}] 🚀  ${taskName}`
     }
 
-    const taskByIndent = new Map<number, string>()
-    for (const [task, indent] of this.runningTasks) {
-      taskByIndent.set(indent, task)
-    }
-
-    const parts: string[] = []
-    for (let i = 0; i <= maxIndent; ++i) {
-      const task = taskByIndent.get(i)
-      if (task === taskName) {
-        parts.push(text ?? taskName)
-        continue
-      }
-
-      if (task === undefined || text === undefined) {
-        parts.push(' ')
-        continue
-      }
-
-      parts.push('|')
-    }
-    return parts.join(this.INDENTATION_STEP).trimEnd()
-  }
-
-  private activeIndents(): number[] {
-    return Array.from(this.runningTasks.values()).sort()
-  }
-
-  private findFreeIndentation(): number {
-    let ret = 0
-
-    const set = new Set(this.activeIndents())
-    while (true) {
-      if (!set.has(ret)) {
-        return ret
-      }
-      ++ret
-    }
+    return `[${this.numEnded}/${this.numStarted}] 🏁  ${taskName}`
   }
 
   begin(taskName: string): string {
-    const indentation = this.findFreeIndentation()
-    this.runningTasks.set(taskName, indentation)
-
+    ++this.numStarted
     return this.getLine(taskName)
   }
 
   ended(taskName: string, verdict: string): string {
+    ++this.numEnded
     const ret = this.getLine(taskName, verdict)
-    this.runningTasks.delete(taskName)
     return ret
   }
 }
