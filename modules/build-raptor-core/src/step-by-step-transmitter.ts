19c2928685519fff5e5af30965a09ea0e8f5f2a9
Itay Maman <94941+imaman@users.noreply.github.com>
2025-01-10 15:42:49 +0200

    trnasmitter multi

--- modules/build-raptor-core/src/step-by-step-transmitter.ts
+++ modules/build-raptor-core/src/step-by-step-transmitter.ts
@@ -7,53 +7,52 @@ import * as util from 'util'
 export class StepByStepTransmitter {
   private readonly steps: Step[] = []
   private readonly promises: Promise<void>[] = []
+  private readonly stepByStepProcessors: StepByStepProcessor[] = []
+  private stepByStepFile: string | undefined = undefined
 
-  private constructor(
-    private readonly stepByStepFile: string,
-    private readonly stepByStepProcessor: StepByStepProcessor | undefined,
-    private readonly logger: Logger,
-  ) {}
+  constructor(private readonly logger: Logger) {}
+
+  setOutputFile(f: string) {
+    this.stepByStepFile = f
+  }
+
+  addProcessor(p: StepByStepProcessor) {
+    this.stepByStepProcessors.push(p)
+  }
 
   transmit(step: Step) {
     const parsed = Step.parse(step)
     this.steps.push(parsed)
 
-    if (this.stepByStepProcessor) {
-      this.promises.push(Promise.resolve(this.stepByStepProcessor(parsed)))
+    for (const p of this.stepByStepProcessors) {
+      this.promises.push(Promise.resolve(p(parsed)))
     }
   }
 
   async close() {
     await Promise.all(this.promises)
+    if (!this.stepByStepFile) {
+      return
+    }
     const parsed = StepByStep.parse(this.steps)
     fs.writeFileSync(this.stepByStepFile, JSON.stringify(parsed))
     this.logger.info(`step by step written to ${this.stepByStepFile}`)
   }
 
-  static async create(
-    stepByStepFile: string,
-    stepByStepProcessorModuleName: string | undefined,
-    logger: Logger,
-    lookFor = 'processor',
-  ) {
-    let processor
-    if (stepByStepProcessorModuleName) {
-      // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-      const imported = loadDynamically(stepByStepProcessorModuleName) as object
-      const temp = Object.entries(imported)
-        .flatMap(([k, v]) => (k === lookFor ? [v] : []))
-        .find(Boolean)
-      if (!temp) {
-        throw new Error(
-          `could not find ${lookFor} in module ${stepByStepProcessorModuleName} which exports ${util.inspect(
-            imported,
-          )}`,
-        )
-      }
-      processor = temp as StepByStepProcessor // eslint-disable-line @typescript-eslint/consistent-type-assertions
+  async dynamicallyLoadProcessor(stepByStepProcessorModuleName: string, lookFor = 'processor') {
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    const imported = loadDynamically(stepByStepProcessorModuleName) as object
+    const temp = Object.entries(imported)
+      .flatMap(([k, v]) => (k === lookFor ? [v] : []))
+      .find(Boolean)
+    if (!temp) {
+      throw new Error(
+        `could not find ${lookFor} in module ${stepByStepProcessorModuleName} which exports ${util.inspect(imported)}`,
+      )
     }
+    const processor = temp as StepByStepProcessor // eslint-disable-line @typescript-eslint/consistent-type-assertions
 
-    logger.info(`processor=${util.inspect(processor)}`)
-    return new StepByStepTransmitter(stepByStepFile, processor, logger)
+    this.logger.info(`processor=${util.inspect(processor)}`)
+    this.addProcessor(processor)
   }
 }
