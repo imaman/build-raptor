28af444e72f14b20b6b1259a4580a8598837d74b
Itay Maman <94941+imaman@users.noreply.github.com>
2025-12-16 23:47:00 +0200

    more

--- modules/build-raptor/tests/zod-to-json5-template.spec.ts
+++ modules/build-raptor/tests/zod-to-json5-template.spec.ts
@@ -8,5 +8,13 @@ describe('zod-to-json5-template', () => {
   a: "",
   b: 0,
 }`)
+    expect(zodToJson5Template(z.object({ a: z.string().array(), b: z.boolean() }), {})).toEqual(`{
+  a: [],
+  b: false,
+}`)
+    expect(zodToJson5Template(z.number().optional(), {})).toEqual(`0`)
+    expect(zodToJson5Template(z.number().nullable(), {})).toEqual(`0`)
+    expect(zodToJson5Template(z.number().nullable().default(5), {})).toEqual(`0`)
+    expect(zodToJson5Template(z.number().default(5).nullable(), {})).toEqual(`0`)
   })
 })
