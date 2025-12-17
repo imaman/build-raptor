509e70bafb49762a8f01ff48cc66b7e681e8a6d8
Itay Maman <94941+imaman@users.noreply.github.com>
2025-12-17 16:55:18 +0200

    linesplitting

--- modules/build-raptor-core/tests/examplify-zod.spec.ts
+++ modules/build-raptor-core/tests/examplify-zod.spec.ts
@@ -98,6 +98,26 @@ describe('examplify-zod', () => {
         `}`,
       ])
     })
+    test('are split if exceed 120 characters', () => {
+      expect(
+        runExamplify(
+          z.object({
+            address: z
+              .string()
+              .describe(
+                'Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal.',
+              ),
+          }),
+        ),
+      ).toEqual([
+        `{`,
+        `  // Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and`,
+        `  // dedicated to the proposition that all men are created equal.`,
+        `  // address: "",`,
+        `}`,
+      ])
+    })
+
     test('nested objects can have a description and also its own properties', () => {
       expect(
         runExamplify(
