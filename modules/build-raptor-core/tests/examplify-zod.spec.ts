45ddf6fe1e6c0c1a053aed9de950a4dc1ded05b4
Itay Maman <94941+imaman@users.noreply.github.com>
2025-12-17 16:55:49 +0200

    lb

--- modules/build-raptor-core/tests/examplify-zod.spec.ts
+++ modules/build-raptor-core/tests/examplify-zod.spec.ts
@@ -105,7 +105,8 @@ describe('examplify-zod', () => {
             address: z
               .string()
               .describe(
-                'Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal.',
+                'Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived ' +
+                  'in Liberty, and dedicated to the proposition that all men are created equal.',
               ),
           }),
         ),
